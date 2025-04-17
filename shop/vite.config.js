cb3f3b7bc305d3ce9b05a9e92b39329e47e55894
yeji424 <yejiborn424@gmail.com>
2025-04-17 15:52:22 +0900

    feat: 자동 import 설정 (jsconfig.json)

--- shop/vite.config.js
+++ shop/vite.config.js
@@ -1,23 +1,16 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { fileURLToPath } from 'url'
+import { defineConfig } from 'vite'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
-    alias: {
-      '@': path.resolve('./src'), // 이 부분이 중요
-      '@components': path.resolve('./src/components'),
-      '@hooks': path.resolve('./src/hooks'),
-      '@pages': path.resolve('./src/pages'),
-      '@utils': path.resolve('./src/utils'),
-      '@assets': path.resolve('./src/assets'),
-      '@api': path.resolve('./src/api'),
-      '@store': path.resolve('./src/store'),
-      // 필요한 다른 경로도 추가 가능
-    },
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
   server: {
     proxy: {
