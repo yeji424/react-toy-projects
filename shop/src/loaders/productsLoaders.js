1fa63858c5350a8bd2d652679ef437acff849dd9
yeji424 <yejiborn424@gmail.com>
2025-04-18 09:33:41 +0900

    feat: productLoaders 필터링 추가

--- shop/src/loaders/productsLoaders.js
+++ shop/src/loaders/productsLoaders.js
@@ -1,7 +1,11 @@
 import { getProductById, getProductsByCategory } from '../api/productsApi'
 
+// 지연 확인용 함수
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+
 export const detailPageLoader = async ({ params }) => {
   try {
+    await delay(1000)
     const product = await getProductById(params.productId)
     console.log('productsLoaders.js:product', product.category)
 
@@ -11,8 +15,13 @@ export const detailPageLoader = async ({ params }) => {
       })
     }
 
+    // 상품 ID의 카테고리 정보와 일치하는 상품들
     const relatedProducts = await getProductsByCategory(product.category, 10)
-    return { product, relatedProducts }
+    // 배열에서 product.id와 일치하는 상품을 제외
+    const filteredRelatedProducts = relatedProducts.filter(
+      relatedProduct => relatedProduct.id !== product.id
+    )
+    return { product, filteredRelatedProducts }
   } catch (e) {
     console.error(e)
     throw new Response('상품 데이터 불러오던 중 오류 발생', { status: e.status || 500 })
